1dd7e7686da60ac8f1d6da13ae4d46c626aec966
Jan Szewczyk <36161455+QueIvan@users.noreply.github.com>
2022-10-14 22:39:32 +0200

    [Structure] Initial commit

---
+++ src/main/java/queivan/rental/RentalApiApplication.java
@@ -0,0 +1,11 @@
+package queivan.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RentalApiApplication.class, args);
+    }
+}
---
+++ src/test/java/queivan/rental/RentalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package queivan.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
